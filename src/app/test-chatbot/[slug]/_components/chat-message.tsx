4199b77fa3ceb693f9ac555b71919400dea804a7
ChaconEfrain <efrachaga@gmail.com>
2025-05-16 17:27:14 -0600

    Add loading animation to chatbot message

--- src/app/test-chatbot/[slug]/_components/chat-message.tsx
+++ src/app/test-chatbot/[slug]/_components/chat-message.tsx
@@ -2,16 +2,15 @@ import React from 'react'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { cn } from '@/lib/utils'
 import { useUser } from '@clerk/nextjs';
-import { Bot } from 'lucide-react';
+import { Bot, Dot } from "lucide-react";
 
 interface ChatMessageProps {
   sender: string;
   message: string;
 }
 
-export default function ChatMessage({sender, message}: ChatMessageProps) {
-
-  const {user} = useUser();
+export default function ChatMessage({ sender, message }: ChatMessageProps) {
+  const { user } = useUser();
 
   return (
     <div
@@ -20,23 +19,31 @@ export default function ChatMessage({sender, message}: ChatMessageProps) {
         sender === "user" ? "justify-end" : "justify-start"
       )}
     >
-      {sender === 'user' ?
+      {sender === "user" ? (
         <Avatar>
           <AvatarImage src={user?.imageUrl} />
           <AvatarFallback>{`${user?.firstName?.[0]}${user?.lastName?.[0]}`}</AvatarFallback>
-        </Avatar> :
+        </Avatar>
+      ) : (
         <Bot />
-      }
+      )}
       <p
-        className={cn(
-          "max-w-[70%] p-3 rounded-lg text-sm font-outfit",
-          sender === "user"
-            ? "bg-primary text-white"
-            : "bg-gray-100 text-gray-800"
-        )}
+        className={cn("max-w-[70%] rounded-lg text-sm font-outfit", {
+          "bg-primary text-white": sender === "user",
+          "bg-gray-100 text-gray-800": sender === "ai",
+          "p-3": message !== "",
+        })}
       >
-        {message}
+        {message !== "" ? (
+          message
+        ) : (
+          <span className="flex">
+            <Dot className="animate-bounce delay-0" />
+            <Dot className="animate-bounce delay-200 -ml-4" />
+            <Dot className="animate-bounce delay-400 -ml-4" />
+          </span>
+        )}
       </p>
     </div>
-  )
+  );
 }
