3cdf957adf31d2e1934699277847c852eef9cb5c
ChaconEfrain <efrachaga@gmail.com>
2025-06-04 09:56:35 -0600

    Edit middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,9 +6,6 @@ export default clerkMiddleware({
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Run for API routes except for webhooks and /embed-styles
-    "/api((?!/webhooks|/embed-styles).*)",
+    "/((?!_next|.*\\..*|embed($|/)|api/embed-styles|api/public|api/webhooks).*).*",
   ],
 };
