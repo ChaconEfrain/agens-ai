3ff62e4fc5af78f7bba8709fbc2130e295b3c425
ChaconEfrain <efrachaga@gmail.com>
2025-06-12 17:53:40 -0600

    Change import syntax

--- src/types/db-transaction.ts
+++ src/types/db-transaction.ts
@@ -2,4 +2,8 @@ import { ExtractTablesWithRelations } from "drizzle-orm";
 import { NodePgQueryResultHKT } from "drizzle-orm/node-postgres";
 import { PgTransaction } from "drizzle-orm/pg-core";
 
-export type Transaction = PgTransaction<NodePgQueryResultHKT, typeof import("c:/Users/efrain.chacon/Proyects/agens-ai/src/db/schema"), ExtractTablesWithRelations<typeof import("c:/Users/efrain.chacon/Proyects/agens-ai/src/db/schema")>>;
+export type Transaction = PgTransaction<
+  NodePgQueryResultHKT,
+  typeof import("@/db/schema"),
+  ExtractTablesWithRelations<typeof import("@/db/schema")>
+>;
