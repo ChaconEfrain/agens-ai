252774e4ddf2d333dda2843d7453717eeb558485
ChaconEfrain <efrachaga@gmail.com>
2025-07-21 16:06:05 -0600

    Create free tier subscription on user creation

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -1,21 +1,27 @@
-import { createUser } from '@/db/user';
-import { verifyWebhook } from '@clerk/nextjs/webhooks'
-import { NextRequest } from 'next/server'
+import { createSubscription } from "@/db/subscriptions";
+import { createUser } from "@/db/user";
+import { verifyWebhook } from "@clerk/nextjs/webhooks";
+import { NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
   try {
-    const evt = await verifyWebhook(req)
-    const eventType = evt.type
+    const evt = await verifyWebhook(req);
+    const eventType = evt.type;
 
-    if (eventType === 'user.created') {
-      const {id, email_addresses, first_name, last_name} = evt.data;
-      const [dbUser] = await createUser({clerkId: id, email: email_addresses[0].email_address, name: (first_name ?? '') + ' ' + (last_name ?? '')})
+    if (eventType === "user.created") {
+      const { id, email_addresses, first_name, last_name } = evt.data;
+      const [dbUser] = await createUser({
+        clerkId: id,
+        email: email_addresses[0].email_address,
+        name: (first_name ?? "") + " " + (last_name ?? ""),
+      });
+      await createSubscription({ userId: dbUser.id });
       return Response.json({ dbUser, status: 200 });
     }
-    
-    return new Response('Event not handled', { status: 200 });
+
+    return new Response("Event not handled", { status: 200 });
   } catch (err) {
-    console.error('Error verifying webhook:', err)
-    return new Response('Error verifying webhook', { status: 400 })
+    console.error("Error verifying webhook:", err);
+    return new Response("Error verifying webhook", { status: 400 });
   }
 }
