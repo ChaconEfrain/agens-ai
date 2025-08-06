e0645f747fa1f29a7eb40711d1120bccc123603a
ChaconEfrain <efrachaga@gmail.com>
2025-08-06 14:47:21 -0600

    Remove unused function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,28 +2,10 @@ import * as XLSX from "xlsx";
 import { Table } from "@tanstack/react-table";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-import DOMPurify from "dompurify";
 import { Message } from "@/db/schema";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
-}
-
-export function sanitizeSvg(svgString: string) {
-  return DOMPurify.sanitize(svgString, {
-    USE_PROFILES: { svg: true },
-    FORBID_TAGS: ["script", "style"],
-    FORBID_ATTR: [
-      "onerror",
-      "onload",
-      "onclick",
-      "onmouseover",
-      "xmlns:xlink",
-      "xmlns",
-      "xlink:href",
-      "style",
-    ],
-  });
 }
 
 export function exportTableToExcel<T>(
