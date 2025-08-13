4b03c8687e4bfd8dfd3b52e49d781a1086e12f93
ChaconEfrain <efrachaga@gmail.com>
2025-08-13 16:11:31 -0600

    Delete unused

--- src/app/test-chatbot/[slug]/_components/chatbot-context.tsx
+++ src/app/test-chatbot/[slug]/_components/chatbot-context.tsx
@@ -20,7 +20,7 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { ArrowRight, Bot, Box, File, Info, Smile, Truck } from "lucide-react";
+import { ArrowRight, Box, File, Info, Smile, Truck } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import type { Business, Chatbot, File as FileSchema } from "@/db/schema";
 import EditContext from "./edit-context";
