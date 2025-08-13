5922895652b126308c375ce1ddc59c79aef4b4ea
ChaconEfrain <efrachaga@gmail.com>
2025-08-13 14:36:47 -0600

    Use chatbot logo as empty state image

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -1,14 +1,29 @@
-import { cn } from '@/lib/utils'
-import { Bot } from 'lucide-react'
-import React from 'react'
+import { cn } from "@/lib/utils";
+import React from "react";
+import ChatbotIcon from "@/assets/chatbot-icon.webp";
 
-export default function Banner({bannerMessage, className}: {bannerMessage: string; className?: string}) {
+export default function Banner({
+  bannerMessage,
+  className,
+}: {
+  bannerMessage: string;
+  className?: string;
+}) {
   return (
-    <div className={cn("flex flex-col gap-2 items-center justify-center", className)}>
-      <Bot className="size-12" />
+    <div
+      className={cn(
+        "flex flex-col gap-2 items-center justify-center",
+        className
+      )}
+    >
+      <img
+        src={ChatbotIcon.src}
+        alt="Chatbot with a grey suit and a vintage look"
+        className="size-20"
+      />
       <p className="text-lg text-muted-foreground font-medium">
         {bannerMessage}
       </p>
     </div>
-  )
+  );
 }
