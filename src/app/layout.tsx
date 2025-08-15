0cf7dec95a52e112ade9d3ff501c553ab7f3386a
ChaconEfrain <efrachaga@gmail.com>
2025-08-15 12:55:33 -0600

    Change favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import Nav from "@/components/nav";
 import { Toaster } from "@/components/ui/sonner";
 import AppWrapper from "@/components/app-wrapper";
 import NextTopLoader from "nextjs-toploader";
-import ChatbotIcon from "@/assets/chatbot-icon.webp";
+import Favicon from "@/assets/favicon.webp";
 
 const outfit = Outfit({
   variable: "--font-outfit",
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
   description:
     "Create, customize, and deploy AI-powered chatbots for your business in minutes. AgensAI helps you train bots with your own data, integrate them into your website, and deliver better customer experiences.",
   icons: {
-    icon: ChatbotIcon.src,
+    icon: Favicon.src,
   },
 };
 
