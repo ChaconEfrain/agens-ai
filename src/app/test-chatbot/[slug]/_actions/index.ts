f1ecacb0833e62c080f4d98dbb84bd5c666a5f68
ChaconEfrain <efrachaga@gmail.com>
2025-08-20 19:05:41 -0600

    Check for sub plan before uploading more files

--- src/app/test-chatbot/[slug]/_actions/index.ts
+++ src/app/test-chatbot/[slug]/_actions/index.ts
@@ -8,6 +8,8 @@ import { getUserByClerkId } from "@/db/user";
 import { getCoherentChunksFromPdf } from "@/services/openai";
 import { updateChatbotPdfTokens } from "@/db/chatbot";
 import { utapi } from "@/services/uploadthing";
+import { getSubscriptionByUserId } from "@/db/subscriptions";
+import { ALLOWED_PDF } from "@/consts/subscription";
 
 export async function updateChatbotFilesAction({
   files,
@@ -18,15 +20,29 @@ export async function updateChatbotFilesAction({
   chatbotId: number;
   businessId: number;
 }) {
-  //TODO: Check for user sub plan
-  const { userId } = await auth();
-
-  if (!userId) throw new Error("No session detected");
-
-  const user = await getUserByClerkId({ clerkId: userId });
-  const result = await utapi.uploadFiles(files);
-
   try {
+    const { userId } = await auth();
+
+    if (!userId) throw new Error("No session detected");
+
+    const user = await getUserByClerkId({ clerkId: userId });
+    const sub = await getSubscriptionByUserId({ userId: user.id });
+
+    if (
+      (sub?.plan === "free" &&
+        files.length + user.files.length >
+          ALLOWED_PDF[sub.plan.toUpperCase() as "FREE"]) ||
+      (sub?.plan === "basic" &&
+        files.length + user.files.length >
+          ALLOWED_PDF[sub.plan.toUpperCase() as "BASIC"])
+    ) {
+      return {
+        success: false,
+        message: `Your current subscription plan only allows ${ALLOWED_PDF[sub.plan.toUpperCase() as "FREE" | "BASIC"]} PDF upload`,
+      };
+    }
+
+    const result = await utapi.uploadFiles(files);
     const filesResult = result.map(({ data }) => ({
       userId: user.id,
       businessId,
